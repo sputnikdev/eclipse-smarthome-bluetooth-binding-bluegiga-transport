7e10277bc73c38a5b5957e0efe7927fc70c08c0c
Vlad Kolotov <vkolotov@gmail.com>
2017-11-29 06:34:32 +1300

    WIP: adding deactivate method to dispose BlueGiga transport Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- src/main/java/org/sputnikdev/esh/binding/bluetooth/transport/bluegiga/activator/BlueGigaActivator.java
+++ src/main/java/org/sputnikdev/esh/binding/bluetooth/transport/bluegiga/activator/BlueGigaActivator.java
@@ -5,20 +5,32 @@ import gnu.io.SerialManager;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.sputnikdev.bluetooth.manager.transport.BluetoothObjectFactory;
 import org.sputnikdev.bluetooth.manager.transport.bluegiga.BluegigaFactory;
 
 @Component(immediate = true, name = "binding.bluetooth.transport.bluegiga.activator")
 public class BlueGigaActivator {
 
+    private BluegigaFactory bluegigaFactory;
+
     @Activate
     public void activate(BundleContext bundleContext) {
         try {
             SerialManager.getInstance();
-            bundleContext.registerService(BluetoothObjectFactory.class.getName(), new BluegigaFactory(), null);
+            // preload native classes
+            bluegigaFactory = new BluegigaFactory();
+            bundleContext.registerService(BluetoothObjectFactory.class.getName(), bluegigaFactory, null);
         } catch (NativeResourceException ex) {
             throw new IllegalStateException("Could not load bluegiga library. Bluegiga transport is not registered: "
                 + ex.getMessage());
+        }
+    }
+
+    @Deactivate
+    public void deactivate(BundleContext bundleContext) {
+        if (bluegigaFactory != null) {
+            bluegigaFactory.dispose();
         }
     }
 
