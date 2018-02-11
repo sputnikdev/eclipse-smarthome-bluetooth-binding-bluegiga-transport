a4bc38a3237011021fa4f5d6690d141b3a13f5ca
Vlad Kolotov <vkolotov@gmail.com>
2018-02-11 18:09:48 +1300

    Logging improvements Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- src/main/java/org/sputnikdev/esh/binding/bluetooth/transport/bluegiga/activator/BlueGigaActivator.java
+++ src/main/java/org/sputnikdev/esh/binding/bluetooth/transport/bluegiga/activator/BlueGigaActivator.java
@@ -6,20 +6,27 @@ import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sputnikdev.bluetooth.manager.transport.BluetoothObjectFactory;
 import org.sputnikdev.bluetooth.manager.transport.bluegiga.BluegigaFactory;
 
 @Component(immediate = true, name = "binding.bluetooth.transport.bluegiga.activator")
 public class BlueGigaActivator {
 
+    private final Logger logger = LoggerFactory.getLogger(BlueGigaActivator.class);
+
     private BluegigaFactory bluegigaFactory;
 
     @Activate
     public void activate(BundleContext bundleContext) {
         try {
-            SerialManager.getInstance();
             // preload native classes
+            logger.debug("Loading native classes");
+            SerialManager.getInstance();
+            logger.debug("Creating a new factory");
             bluegigaFactory = new BluegigaFactory();
+            logger.debug("Factory created: {}", Integer.toHexString(bluegigaFactory.hashCode()));
             bundleContext.registerService(BluetoothObjectFactory.class.getName(), bluegigaFactory, null);
         } catch (NativeResourceException ex) {
             throw new IllegalStateException("Could not load bluegiga library. Bluegiga transport is not registered: "
